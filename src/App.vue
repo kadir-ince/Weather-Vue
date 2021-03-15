37939a77ffbdd2b625f0e3cb885f693238e6b174
Kadir Ince <you.can.send@pm.me>
2021-03-15 06:25:54 +0300

    Connect API && Get Weather Datas

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,178 @@
-<template>
-  <div id="app"></div>
-</template>
-
 <script>
 export default {
   name: "App",
-  components: {}
+  data() {
+    return {
+      api_key: "15292c790c10e3c0f42c41c93d6de0ce",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {}
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key === "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then(res => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    }
+  }
 };
 </script>
 
-<style>
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search Anywhere..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="weather-box">
+            <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+            <div class="weather">{{ weather.weather[0].main }}</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
 
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0 16px 0;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.25);
+  background: rgba(255, 255, 255, 0.5) none;
+  border-radius: 0 16px 0 16px;
+  transition: 0.4s;
+}
+
+@media (max-width: 420px) {
+  #app {
+    background-image: url("./assets/cold-bg.jpg");
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;
+  }
+
+  main {
+    min-height: 100vh;
+    padding: 25px;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.25),
+      rgba(0, 0, 0, 0.75)
+    );
+  }
+
+  .search-box {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+}
+
+@media (min-width: 420px) {
+  #app {
+    background-image: url("./assets/weather.jpg");
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;
+  }
+
+  main {
+    min-height: 100vh;
+    padding: 25px;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.25),
+      rgba(0, 0, 0, 0.75)
+    );
+  }
+  .search-box {
+    width: 55%;
+    margin: 0 auto;
+    padding-bottom: 30px;
+  }
+}
 </style>
